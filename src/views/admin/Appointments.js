b6879c72a98b1ffb65ce015189188397e12c52ba
Renz B Ramos <renzbramos@gmail.com>
2022-10-20 17:03:17 +0800

    [add] checklist changes initial

--- src/views/admin/Appointments.js
+++ src/views/admin/Appointments.js
@@ -6,19 +6,23 @@ import {
   Card,
   Table,
   Container,
-  Button,
-  Badge,
   Modal,
   ModalHeader,
   ModalBody,
   ModalFooter,
   FormGroup,
   Col,
-} from "react-bootstrap";
+  Button,
+  Badge,
+  CardHeader,
+  CardTitle,
+  CardBody,
+  Input,
+  Label,
+} from "reactstrap";
 
 // Alert Dialogs
 import Swal from "sweetalert2";
-import { Input, Label } from "reactstrap";
 
 const axios = require("axios").default;
 
@@ -28,8 +32,9 @@ function Appointments() {
   const [data, setData] = useState([]);
   const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const [checlist1, setCheckList1] = useState(false);
+
+  const toggleModal = () => setShow(!show);
 
   useEffect(() => {
     axios
@@ -42,24 +47,26 @@ function Appointments() {
   const handleApproveAppointment = (e, data) => {
     e.preventDefault();
 
-    axios
-      .put(
-        process.env.REACT_APP_API_URL +
-          "/schedule/" +
-          data.id +
-          "?decision=PASSED"
-      )
-      .then(() => {
-        Swal.fire({
-          icon: "success",
-          title: `SUCCESS! `,
-          text: `Record approved! We will redirect you now to PICK-UP form`,
-        }).then((result) => {
-          if (result.isConfirmed) {
-            history.push(`/admin/pick-up/${data.userId}/${data.petCode}`);
-          }
-        });
-      });
+    toggleModal();
+
+    // axios
+    //   .put(
+    //     process.env.REACT_APP_API_URL +
+    //       "/schedule/" +
+    //       data.id +
+    //       "?decision=PASSED"
+    //   )
+    //   .then(() => {
+    //     Swal.fire({
+    //       icon: "success",
+    //       title: `SUCCESS! `,
+    //       text: `Record approved! We will redirect you now to PICK-UP form`,
+    //     }).then((result) => {
+    //       if (result.isConfirmed) {
+    //         history.push(`/admin/pick-up/${data.userId}/${data.petCode}`);
+    //       }
+    //     });
+    //   });
   };
 
   const handleDenyAppointment = (e, id) => {
@@ -85,11 +92,11 @@ function Appointments() {
   return (
     <Container fluid>
       <Card className="strpied-tabled-with-hover">
-        <Card.Header>
-          <Card.Title as="h4">Admin Interviews</Card.Title>
+        <CardHeader>
+          <CardTitle as="h4">Admin Interviews</CardTitle>
           <p className="card-category">List of interviews</p>
-        </Card.Header>
-        <Card.Body className="table-full-width table-responsive px-0">
+        </CardHeader>
+        <CardBody className="table-full-width table-responsive px-0">
           <Table className="table-hover table-striped">
             <thead>
               <tr>
@@ -144,143 +151,13 @@ function Appointments() {
                         <>
                           <Button
                             className="btn btn-success mr-2"
-                            // onClick={handleShow}
                             onClick={(e) =>
                               handleApproveAppointment(e, entry.id)
                             }
                           >
                             PASSED
                           </Button>
-                          <Modal
-                            show={show}
-                            onHide={handleClose}
-                            animation={false}
-                          >
-                            <Modal.Header closeButton>
-                              <Modal.Title>Modal heading</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>
-                              <FormGroup check>
-                                <Col>
-                                  <Input type="checkbox" />
-                                  <Label
-                                    check
-                                    className="font-weight-bold"
-                                    style={{ fontSize: 18 }}
-                                  >
-                                    Is the user eligible to adapt a pet?
-                                  </Label>
-                                </Col>
 
-                                <Col>
-                                  <Input type="checkbox" />
-                                  <Label
-                                    check
-                                    className="font-weight-bold"
-                                    style={{ fontSize: 18 }}
-                                  >
-                                    Is the user has valid i.d?
-                                  </Label>
-                                </Col>
-
-                                <Col>
-                                  <Input type="checkbox" />
-                                  <Label
-                                    check
-                                    className="font-weight-bold"
-                                    style={{ fontSize: 18 }}
-                                  >
-                                    Is the user has job or finacially stable?
-                                  </Label>
-                                </Col>
-
-                                <Col>
-                                  <Input type="checkbox" />
-                                  <Label
-                                    check
-                                    className="font-weight-bold"
-                                    style={{ fontSize: 18 }}
-                                  >
-                                    Is it safe for the pet to live with him/her
-                                    place?
-                                  </Label>
-                                </Col>
-
-                                <Col>
-                                  <Input type="checkbox" />
-                                  <Label
-                                    check
-                                    className="font-weight-bold"
-                                    style={{ fontSize: 18 }}
-                                  >
-                                    Has the adopte ever owned a pet before?
-                                  </Label>
-                                </Col>
-
-                                <Col>
-                                  <Input type="checkbox" />
-                                  <Label
-                                    check
-                                    className="font-weight-bold"
-                                    style={{ fontSize: 18 }}
-                                  >
-                                    If she/he is renting a property, is she/he
-                                    provided the building's pet policy?
-                                  </Label>
-                                </Col>
-
-                                <Col>
-                                  <Input type="checkbox" />
-                                  <Label
-                                    check
-                                    className="font-weight-bold"
-                                    style={{ fontSize: 18 }}
-                                  >
-                                    Is he/she understand the responsibility of
-                                    adopting a life?
-                                  </Label>
-                                </Col>
-
-                                <Col>
-                                  <Input type="checkbox" />
-                                  <Label
-                                    check
-                                    className="font-weight-bold"
-                                    style={{ fontSize: 18 }}
-                                  >
-                                    Is he/she comitted to taking thier animal to
-                                    get a checkup once a month?
-                                  </Label>
-                                </Col>
-
-                                <Col>
-                                  <Input type="checkbox" />
-                                  <Label
-                                    check
-                                    className="font-weight-bold"
-                                    style={{ fontSize: 18 }}
-                                  ></Label>
-                                </Col>
-
-                                <Col>
-                                  <Input type="checkbox" />
-                                  <Label
-                                    check
-                                    className="font-weight-bold"
-                                    style={{ fontSize: 18 }}
-                                  ></Label>
-                                </Col>
-                              </FormGroup>
-                            </Modal.Body>
-                            <Modal.Footer>
-                              <Button variant="secondary" onClick={handleClose}>
-                                Close
-                              </Button>
-                              <Button variant="primary" onClick={handleClose}>
-                                Save Changes
-                              </Button>
-                            </Modal.Footer>
-                          </Modal>
                           <Button
                             className="btn btn-danger"
                             onClick={(e) => handleDenyAppointment(e, entry.id)}
@@ -295,8 +172,123 @@ function Appointments() {
               })}
             </tbody>
           </Table>
-        </Card.Body>
+        </CardBody>
       </Card>
+
+      <Modal className="pb-5" isOpen={show} size="lg">
+        <ModalHeader toggle={toggleModal}>
+          <span>Passed Checklist</span>
+        </ModalHeader>
+        <ModalBody>
+          <div className="table-full-width table-responsive p-3">
+            <FormGroup>
+              <Input
+                id="checkList1"
+                type="checkbox"
+                className="mt-2 pointer"
+                onChange={(e) => setCheckList1(e.target.checked)}
+              />
+              <Label
+                for="checkList1"
+                check
+                className="font-weight-bold"
+                style={{ fontSize: 18 }}
+              >
+                Is the user eligible to adapt a pet?
+              </Label>
+            </FormGroup>
+
+            <FormGroup>
+              <Input id="2" type="checkbox" className="mt-2 pointer" />
+              <Label
+                for="2"
+                check
+                className="font-weight-bold"
+                style={{ fontSize: 18 }}
+              >
+                Is the user has valid i.d?
+              </Label>
+            </FormGroup>
+
+            <FormGroup>
+              <Input id="3" type="checkbox" className="mt-2 pointer" />
+              <Label
+                for="3"
+                check
+                className="font-weight-bold"
+                style={{ fontSize: 18 }}
+              >
+                Is the user has job or finacially stable?
+              </Label>
+            </FormGroup>
+
+            <FormGroup>
+              <Input type="checkbox" className="mt-2 pointer" />
+              <Label
+                check
+                className="font-weight-bold"
+                style={{ fontSize: 18 }}
+              >
+                Is it safe for the pet to live with him/her place?
+              </Label>
+            </FormGroup>
+
+            <FormGroup>
+              <Input type="checkbox" className="mt-2 pointer" />
+              <Label
+                check
+                className="font-weight-bold"
+                style={{ fontSize: 18 }}
+              >
+                Has the adopte ever owned a pet before?
+              </Label>
+            </FormGroup>
+
+            <FormGroup>
+              <Input type="checkbox" className="mt-2 pointer" />
+              <Label
+                check
+                className="font-weight-bold"
+                style={{ fontSize: 18 }}
+              >
+                If she/he is renting a property, is she/he provided the
+                building's pet policy?
+              </Label>
+            </FormGroup>
+
+            <FormGroup>
+              <Input type="checkbox" className="mt-2 pointer" />
+              <Label
+                check
+                className="font-weight-bold"
+                style={{ fontSize: 18 }}
+              >
+                Is he/she understand the responsibility of adopting a life?
+              </Label>
+            </FormGroup>
+
+            <FormGroup>
+              <Input type="checkbox" className="mt-2 pointer" />
+              <Label
+                check
+                className="font-weight-bold"
+                style={{ fontSize: 18 }}
+              >
+                Is he/she comitted to taking thier animal to get a checkup once
+                a month?
+              </Label>
+            </FormGroup>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button variant="secondary" onClick={toggleModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={toggleModal}>
+            Save Changes
+          </Button>
+        </ModalFooter>
+      </Modal>
     </Container>
   );
 }
